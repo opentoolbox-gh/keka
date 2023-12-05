68e1f3905a96c6d15a1900042e2070694c18c554
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2023-12-05 11:01:00 +0200

    feat: search by mostly used tags

--- src/components/common/tag.tsx
+++ src/components/common/tag.tsx
@@ -1,14 +1,18 @@
-import { FC } from 'react';
+import { FC, MouseEvent } from 'react';
 
 export interface TagProps {
   tag: string;
-  count ?: number;
+  count?: number;
+  onClick?: (event: MouseEvent<HTMLDivElement>) => void;
 }
 
-const Tag: FC<TagProps> = ({tag, count}) => {
+const Tag: FC<TagProps> = ({ tag, count, onClick }) => {
   return (
-    <div className="flex items-center bg-blue-500 text-white rounded-md p-2 hover:cursor-pointer">
-      <span className=' text-lg font-bold'>{tag}</span>
+    <div
+      className="flex items-center bg-blue-500 text-white rounded-md p-2 hover:cursor-pointer"
+      onClick={onClick}
+    >
+      <span className='text-lg font-bold'>{tag}</span>
       <div className="ml-2 bg-white text-blue-500 rounded-full p-1 font-semibold">
         {count}
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { FC, useEffect, useState } from "react";
 import Tag, { TagProps } from "@/components/common/tag";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 
 import ProfileOverview, { ProfileProps } from "@/components/home/min-profile";
 import axios from "axios";
@@ -17,7 +17,10 @@ const Home: FC = () => {
         const response = await axios.get(`${apisInfo.url}/api/users`);
         setUsers(response.data);
       } catch (error: any) {
-        toast.error(error.message, { position: toast.POSITION.TOP_CENTER, autoClose: 5000 });
+        toast.error(error.message, {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 5000,
+        });
       }
     };
 
@@ -26,30 +29,57 @@ const Home: FC = () => {
         const response = await axios.get(`${apisInfo.url}/api/mostUsedTags`);
         setTags(response.data);
       } catch (error: any) {
-        toast.error(error.message, { position: toast.POSITION.TOP_CENTER, autoClose: 5000 });
+        toast.error(error.message, {
+          position: toast.POSITION.TOP_CENTER,
+          autoClose: 5000,
+        });
       }
-    }
-  
+    };
+
     fetchUsers();
     fetchTags();
   }, []);
+
+  const searchByTag = async (tag: string): Promise<void> => {
+    try {
+      const response = await axios.get(
+        `${apisInfo.url}/api/users/tag/${tag.trim()}`
+      );
+      setUsers(response.data);
+    } catch (error: any) {
+      toast.error(error.message, {
+        position: toast.POSITION.TOP_CENTER,
+        autoClose: 5000,
+      });
+    }
+  };
+
   return (
     <>
       {/* add mostly used tags */}
       <div className="flex items-center justify-center gap-6  mt-8">
         {tags?.map((tag) => {
-          const randomKey: string = `tag-id-${Math.floor(Math.random() * 1000)}`
-          return <Tag tag={tag.tag} count={tag.count} key={randomKey}/>;
+          const randomKey: string = `tag-id-${Math.floor(
+            Math.random() * 1000
+          )}`;
+          return (
+            <Tag
+              tag={tag.tag}
+              count={tag.count}
+              key={randomKey}
+              onClick={() => {
+                searchByTag(tag.tag);
+              }}
+            />
+          );
         })}
-      </div>  
+      </div>
 
       {/* Add profiles */}
       <div className="flex flex-wrap justify-center gap-4 mt-12 z-0">
-        {
-          users?.map((user: ProfileProps) => {
-            return <ProfileOverview  user={user} key={user.id} />
-          })
-        }
+        {users?.map((user: ProfileProps) => {
+          return <ProfileOverview user={user} key={user.id} />;
+        })}
       </div>
     </>
   );
